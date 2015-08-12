64a6094c3eb48eb77bccdcc544705e885a59c587
Humdaan Mustafa <humishum@gmail.com>
2015-08-12 01:05:44 -0700

    boilerplate#2

--- script.js
+++ script.js
@@ -3,3 +3,22 @@
 // GetGit: Simple Tool to scrape git commits n' stuff
 
 //Requires
+var express = require('express');
+var fs      = require('fs');
+var request = request('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+//This is where the magic happens
+app.get('/scrape',function(req,res){
+  
+    
+})
+
+// app is on 8080
+app.listen('8080')
+//confirmation that app is running
+console.log('Magic is happening on 8080');
+
+//idk why i need this tbh
+exports = module.exports = app
